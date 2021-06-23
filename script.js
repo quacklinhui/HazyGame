a0b4e636bea4d4a54246cc4127015543f7242185
queklinhui12@gmail.com <queklinhui12@gmail.com>
2021-06-24 03:59:18 +0800

    yay

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function hello(){
-    // alert(document.getElementById("tiktokCode").value);
-    // document.getElementById("video1").style.opacity = 0;
-    // document.getElementById("video2").style.opacity = 0;
-    // console.log("This is working 1");
     if (document.getElementById("tiktokCode").value=="first"){
         document.getElementById("video2").style.opacity = 0;
     } else if(document.getElementById("tiktokCode").value=="second"){
