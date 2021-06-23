60d6b1a064474b11fe67259c3cb410210f2cebbb
queklinhui12@gmail.com <queklinhui12@gmail.com>
2021-06-24 03:38:12 +0800

    the password will reduce the haze

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){return}
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },300);
-}
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+function hello(){
+    // alert(document.getElementById("tiktokCode").value);
+    // document.getElementById("video1").style.opacity = 0;
+    // document.getElementById("video2").style.opacity = 0;
+    // console.log("This is working 1");
+    if (document.getElementById("tiktokCode").value=="first"){
+        document.getElementById("video2").style.opacity = 0;
+    } else if(document.getElementById("tiktokCode").value=="second"){
+        console.log("This is working");
+        document.getElementById("video3").style.opacity = 0;
     }
-}, 10);
+}
