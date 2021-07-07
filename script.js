9596de172a9d3a8518475aa67fe770a3a09f1845
queklinhui12@gmail.com <queklinhui12@gmail.com>
2021-07-07 22:26:53 +0800

    rainwillcome

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function hello(){
-    if (document.getElementById("tiktokCode").value=="first"){
+    if (document.getElementById("tiktokCode").value=="rain"){
         playAudio2()
         document.getElementById("video2").style.opacity = 0;
-    } else if(document.getElementById("tiktokCode").value=="second"){
+    } else if(document.getElementById("tiktokCode").value=="will"){
         document.getElementById("video3").style.opacity = 0;
         playAudio3()
-        document.getElementById("thecode").style.visibility = "visible";
+        document.getElementById("thecode").style.visibility = "come";
     } else if(document.getElementById("tiktokCode").value=="third"){
         playAudio1()
         document.getElementById("video1").style.opacity = 0; //change this to the suitcase
